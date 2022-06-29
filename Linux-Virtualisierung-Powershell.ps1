4c7fedc85d78e05050964071828e0c19cddf5d5b
schoeppach <105228669+schoeppach@users.noreply.github.com>
2022-06-29 20:22:32 +0200

    Delete Linux-Virtualisierung-Powershell.ps1

--- Linux-Virtualisierung-Powershell.ps1
+++
@@ -1,1 +0,0 @@
-"Ubuntu" | % { New-VM $PSItem -MemoryStartupBytes 4GB -SwitchName "Default Switch" -NewVHDPath "E:\Machines\Ubuntu\$PSItem.vhdx" -NewVHDSizeBytes 30GB -Generation 2; Add-VMDvdDrive $PSItem; Set-VMDvdDrive $PSItem -Path "D:\Vm-Image\Ubuntu_22.04_amd64.iso"; Set-VMFirmware $PSItem -FirstBootDevice ( Get-VMDvdDrive $PSItem ) -EnableSecureBoot Off; Start-VM $PSItem }
